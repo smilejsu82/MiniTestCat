ef524f736f243f54940cd82a3ddfef1e28ca4f68
smilejsu <smilejsu82@gmail.com>
2022-01-20 13:12:42 +0900

    2단계 완료
    UI에 Text에 깃발과 거리 표시 한다

--- MiniTest/Assets/Scripts/Cat.cs
+++ MiniTest/Assets/Scripts/Cat.cs
@@ -1,26 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Cat : MonoBehaviour
 {
     public float speed;
     private GameObject flagGo;
     private bool canMove = false;
-
+    private Text distanceText;
+    private float distance;
     void Start()
     {
-        this.flagGo = GameObject.Find("flag");    
+        this.flagGo = GameObject.Find("flag");
+        this.distanceText = GameObject.Find("distanceText").GetComponent<Text>();
+        this.CalcDistance();
+        this.UpdateDistanceText();
     }
 
     void Update()
     {
         if (this.canMove) {
-            float distance = Vector2.Distance(this.transform.position, this.flagGo.transform.position);
+            this.CalcDistance();
+            this.UpdateDistanceText();
 
             if (distance > 0.2)
                 this.transform.Translate(Vector2.right * this.speed * Time.deltaTime);
         }
+    }
+
+    private void CalcDistance() {
+        this.distance = Vector2.Distance(this.transform.position, this.flagGo.transform.position);
+    }
+
+    private void UpdateDistanceText() {
+        this.distanceText.text = distance.ToString() + "m";
     }
 
     public void MoveStart() {
