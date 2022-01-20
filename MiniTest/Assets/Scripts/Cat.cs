4606eb2a5f02744e5c73a41df304c2f2cf4e340e
smilejsu <smilejsu82@gmail.com>
2022-01-20 13:26:22 +0900

    3단계 완료 고양이에게 HP속성을 부여 한다 적당한 trap icon을 찾아서 가져와서 설치 한다 깃발까지 가는 도중에 trap과 충돌하면 피해를 한번 입고 그냥 가던길 계속 간다 UI상에 HP게이지를 표시한다

--- MiniTest/Assets/Scripts/Cat.cs
+++ MiniTest/Assets/Scripts/Cat.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.UI;
 
 public class Cat : MonoBehaviour
@@ -10,9 +11,22 @@ public class Cat : MonoBehaviour
     private bool canMove = false;
     private Text distanceText;
     private float distance;
+    public float hp;
+    public float maxHp;
+    public float radius = 1.0f;
+    public float trapRadius = 1.0f;
+    public GameObject trapGo;
+    private bool canHit = true;
+    public UnityAction OnHit;
+
     void Start()
     {
+        this.maxHp = 10;
+        this.hp = this.maxHp;
+
         this.flagGo = GameObject.Find("flag");
+        this.trapGo = GameObject.Find("trap");
+
         this.distanceText = GameObject.Find("distanceText").GetComponent<Text>();
         this.CalcDistance();
         this.UpdateDistanceText();
@@ -24,9 +38,28 @@ public class Cat : MonoBehaviour
             this.CalcDistance();
             this.UpdateDistanceText();
 
+            var dis = Vector2.Distance(this.trapGo.transform.position, this.transform.position);
+            if (dis < this.radius + this.trapRadius) {
+                if (this.canHit) {
+                    Debug.Log("¾Æ¾æ!");
+                    this.hp -= 1;
+                    this.canHit = false;
+                    this.OnHit();
+                }
+            }
+
+            
             if (distance > 0.2)
                 this.transform.Translate(Vector2.right * this.speed * Time.deltaTime);
         }
+    }
+
+    public float GetMaxHp() {
+        return this.maxHp;
+    }
+
+    public float GetHp() {
+        return this.hp;
     }
 
     private void CalcDistance() {
--- MiniTest/Assets/Scripts/GameMain.cs
+++ MiniTest/Assets/Scripts/GameMain.cs
@@ -7,11 +7,20 @@ public class GameMain : MonoBehaviour
 {
     public Button btnMoveStart;
     public Button btnMoveStop;
+    public Image hpGauge;
 
     public Cat cat;
 
     void Start()
     {
+        cat.OnHit = () =>
+        {
+            //UI를 갱신 한다 
+            //cat의 hp와 maxHp를 알아야 한다 
+            var per = this.cat.GetHp() / this.cat.GetMaxHp();
+            this.hpGauge.fillAmount = per;  //0 ~ 1
+        };
+
         this.btnMoveStart.onClick.AddListener(()=> {
             cat.MoveStart();
         });
